6fd913b6fea5bb100723d9fb14d9f0f4e4daf719
joinwu <joinwu@tencent.com>
2021-02-20 18:16:07 +0800

    Site updated: 2021-02-20 18:16:03

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 	switchTreeOrIndex();
 	scrollToTop();
 	pageScroll();
+    wrapImageWithFancyBox();
 });
 // 页面滚动
 function pageScroll(){
@@ -307,4 +308,42 @@ function scrollOff(){
 	$header.removeClass('header-on');
 	$footer.addClass('off');
 	$footer.removeClass('header-on');
+}
+/**
+ * Wrap images with fancybox support.
+ */
+function wrapImageWithFancyBox() {
+    $('img').not('.sidebar-image img').not('#author-avatar img').not(".mdl-menu img").not(".something-else-logo img").not('[title=notice]').each(function() {
+        var $image = $(this);
+        var imageCaption = $image.attr('alt');
+        var $imageWrapLink = $image.parent('a');
+
+        if ($imageWrapLink.size() < 1) {
+            var src = this.getAttribute('src');
+            var idx = src.lastIndexOf('?');
+            if (idx != -1) {
+                src = src.substring(0, idx);
+            }
+            $imageWrapLink = $image.wrap('<a href="' + src + '"></a>').parent('a');
+        }
+
+        $imageWrapLink.attr('data-fancybox', 'images');
+        if (imageCaption) {
+            $imageWrapLink.attr('data-caption', imageCaption);
+        }
+
+    });
+
+    $('[data-fancybox="images"]').fancybox({
+      buttons : [ 
+        'slideShow',
+		'thumbs',
+        'zoom',
+        'fullScreen',
+        'close'
+      ],
+      thumbs : {
+        autoStart : false
+      }
+    });
 }
